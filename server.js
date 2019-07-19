4d586dfc1ad5b5e9f0f55d13412cb1eb3a1138ae
Blake Mitchell <mblakeandrew@gmail.com>
2019-07-19 09:50:02 -0700

    post route for acitons is working

--- server.js
+++ server.js
@@ -19,5 +19,22 @@ server.post("/api/projects", (req, res) => {
     res.status(500).json({error: error.message})
   })
 })
+server.post("/api/actions/:id", (req, res) => {
+  const {action_description} = req.body;
+  const {action_notes} = req.body;
+  const {action_completed} = req.body;
+  const project_id = req.params.id;
+  // console.log("action:", action);
+  // console.log("project id:", project_id);
+  db("actions")
+  .insert({action_description, action_notes, action_completed, project_id}, "id")
+  .then(action => {
+    res.status(200).json(action);
+  })
+  .catch(error => {
+    res.status(500).json({error: error.message})
+  })
+})
+
 
 module.exports = server;
